fd9bf8a5e66fb7408489d7291f14af3553054fd2
Mazen Bdewi <mazen.bdewi@gmail.com>
2025-11-25 10:39:52 +0300

    update user

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,6 +48,7 @@ class User extends Authenticatable implements FilamentUser, HasAvatar, HasPasske
         'affiliation',
         'bio',
         'user_type',
+        'email_verified_at',
 
     ];
 
